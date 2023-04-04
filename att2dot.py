b95a0953d2c30638d5fe9fe0c2d81e7837ab54d8
nsingh475 <87938938+nsingh475@users.noreply.github.com>
2023-04-03 22:08:12 -0400

    Add files via upload

--- att2dot.py
+++ att2dot.py
@@ -1,9 +1,15 @@
 import sys
 
 print('digraph G { rankdir="LR"')
-print('node [fontname="Tahoma",shape=circle,fontsize=14,fixedsize=true,fillcolor="grey",style=filled]')
-print('edge [fontname="FreeMono",fontsize=14]')
-for line in sys.stdin.readlines():
+print('node [fontname="Lohit Devanagari",shape=circle,fontsize=14,fixedsize=true,fillcolor="grey",style=filled]')
+print('edge [fontname="Lohit Devanagari",fontsize=14]')
+
+# Read input as bytes and decode using UTF-8 encoding
+input_bytes = sys.stdin.buffer.read()
+input_text = input_bytes.decode('utf-8')
+
+# Split the input into lines and process each line
+for line in input_text.split('\n'):
     line = line.strip()
     row = line.split('\t')
     if len(row) >= 4:
@@ -12,3 +18,4 @@ for line in sys.stdin.readlines():
     elif len(row) == 2: # Final state
         print('%s [label="%s",shape=doublecircle];' % (row[0], row[0]))
         print('}')
+
